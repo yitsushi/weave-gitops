a45b65dba5e42e063d3a89300104c6bff279d080
Jerry Jackson <jerry@weave.works>
2021-04-14 16:42:01 -0600

    initial working add

--- pkg/fluxops/fluxops.go
+++ pkg/fluxops/fluxops.go
@@ -1,13 +1,16 @@
 package fluxops
 
 import (
-	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/exec"
-	"strings"
+	"path/filepath"
 
+	"github.com/weaveworks/weave-gitops/pkg/status"
+	"github.com/weaveworks/weave-gitops/pkg/utils"
 	"github.com/weaveworks/weave-gitops/pkg/version"
+	"sigs.k8s.io/yaml"
 )
 
 var (
@@ -38,28 +41,82 @@ func CallFlux(arglist string) ([]byte, error) {
 
 func defaultFluxHandler(arglist string) ([]byte, error) {
 	initFluxBinary()
-	return CallCommand(fmt.Sprintf("%s %s", fluxBinary, arglist))
+	return utils.CallCommand(fmt.Sprintf("%s %s", fluxBinary, arglist))
 }
 
-func CallCommand(cmdstr string) ([]byte, error) {
-	cmd := exec.Command("sh", "-c", Escape(cmdstr))
-	return cmd.CombinedOutput()
+func Bootstrap(owner, repoName string) error {
+	isOrg, err := isOrganization(owner)
+	if err != nil {
+		return err
+	}
+	if isOrg {
+		if _, err := CallFlux(fmt.Sprintf("bootstrap github --owner=%s --repository=%s", owner, repoName)); err != nil {
+			return err
+		}
+	} else {
+		if _, err := CallFlux(fmt.Sprintf("bootstrap github --owner=%s --repository=%s --branch=main --private=false --personal=true", owner, repoName)); err != nil {
+			return err
+		}
+	}
+	if status.GetClusterStatus() != status.FluxInstalled {
+		return fmt.Errorf("Failed to install flux")
+	}
+
+	return nil
 }
 
-func CallCommandSeparatingOutputStreams(cmdstr string) ([]byte, []byte, error) {
-	cmd := exec.Command("sh", "-c", Escape(cmdstr))
-	var stdout, stderr bytes.Buffer
-	cmd.Stdout = &stdout
-	cmd.Stderr = &stderr
-	err := cmd.Run()
-	return stdout.Bytes(), stderr.Bytes(), err
+func GetOwnerFromEnv() (string, error) {
+	// check for github username
+	user, okUser := os.LookupEnv("GITHUB_ORG")
+	if okUser {
+		return user, nil
+	}
+
+	return getUserFromHubCredentials()
 }
 
-func CallCommandForEffect(cmdstr string) error {
-	cmd := exec.Command("sh", "-c", Escape(cmdstr))
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+func GetRepoName() (string, error) {
+	clusterName, err := status.GetClusterName()
+	if err != nil {
+		return "", err
+	}
+	return clusterName + "-wego", nil
+}
+
+func getUserFromHubCredentials() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	// check for existing ~/.config/hub
+	config, err := ioutil.ReadFile(filepath.Join(homeDir, ".config", "hub"))
+	if err != nil {
+		return "", err
+	}
+
+	data := map[string]interface{}{}
+	err = yaml.Unmarshal(config, &data)
+	if err != nil {
+		return "", err
+	}
+
+	return data["github.com"].([]interface{})[0].(map[string]interface{})["user"].(string), nil
+}
+
+func isOrganization(owner string) (bool, error) {
+	token := os.Getenv("GITHUB_TOKEN")
+	response, _, err := utils.CallCommandSeparatingOutputStreams(fmt.Sprintf("curl -u %s:%s https://api.github.com/orgs/%s", owner, token, owner))
+
+	if err != nil {
+		return false, err
+	}
+	var data map[string]interface{}
+	err = json.Unmarshal(response, &data)
+	if err != nil {
+		return false, err
+	}
+	return data["message"] != "Not Found", nil
 }
 
 func initFluxBinary() {
@@ -71,8 +128,4 @@ func initFluxBinary() {
 		}
 		fluxBinary = fluxPath
 	}
-}
-
-func Escape(cmd string) string {
-	return strings.ReplaceAll(cmd, "'", "'\"'\"'")
 }
