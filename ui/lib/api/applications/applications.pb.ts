ca67b9aabb1ae57485b77a5c022489dda79f2974
Jordan Pellizzari <jordan@weave.works>
2021-07-09 14:01:28 -0700

    Add AddApplication method to server

--- ui/lib/api/applications/applications.pb.ts
+++ ui/lib/api/applications/applications.pb.ts
@@ -5,6 +5,17 @@
 */
 
 import * as fm from "./fetch.pb"
+
+export enum AddApplicationRequestDeploymentType {
+  KUSTOMIZE = "KUSTOMIZE",
+  HELM = "HELM",
+}
+
+export enum AddApplicationRequestSourceType {
+  GIT = "GIT",
+  CHART = "CHART",
+}
+
 export type Application = {
   name?: string
   path?: string
@@ -28,11 +39,32 @@ export type GetApplicationResponse = {
   application?: Application
 }
 
+export type AddApplicationRequest = {
+  name?: string
+  url?: string
+  path?: string
+  branch?: string
+  deploymentType?: AddApplicationRequestDeploymentType
+  chart?: string
+  sourceType?: AddApplicationRequestSourceType
+  appConfigUrl?: string
+  namespace?: string
+  dryRun?: boolean
+  autoMerge?: boolean
+}
+
+export type AddApplicationResponse = {
+  application?: Application
+}
+
 export class Applications {
   static ListApplications(req: ListApplicationsRequest, initReq?: fm.InitReq): Promise<ListApplicationsResponse> {
     return fm.fetchReq<ListApplicationsRequest, ListApplicationsResponse>(`/v1/applications?${fm.renderURLSearchParams(req, [])}`, {...initReq, method: "GET"})
   }
   static GetApplication(req: GetApplicationRequest, initReq?: fm.InitReq): Promise<GetApplicationResponse> {
     return fm.fetchReq<GetApplicationRequest, GetApplicationResponse>(`/v1/applications/${req["name"]}?${fm.renderURLSearchParams(req, ["name"])}`, {...initReq, method: "GET"})
+  }
+  static AddApplication(req: AddApplicationRequest, initReq?: fm.InitReq): Promise<AddApplicationResponse> {
+    return fm.fetchReq<AddApplicationRequest, AddApplicationResponse>(`/v1/applications`, {...initReq, method: "POST"})
   }
 }
