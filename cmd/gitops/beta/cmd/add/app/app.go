24897abfab9e51fa453d6074b0cb8acf53849df7
James Wilson <jwils22@gmail.com>
2022-01-11 15:41:08 -0500

    Added interactive questions to add app

--- cmd/gitops/beta/cmd/add/app/app.go
+++ cmd/gitops/beta/cmd/add/app/app.go
@@ -5,9 +5,11 @@ This file is part of the Weave GitOps CLI.
 package app
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/weaveworks/weave-gitops/cmd/internal"
@@ -15,8 +17,15 @@ import (
 	"github.com/weaveworks/weave-gitops/core/repository"
 )
 
+const (
+	namespaceFlag   = "namespace"
+	descriptionFlag = "description"
+)
+
 type Params struct {
-	Name string
+	Name        string
+	Namespace   string
+	Description string
 }
 
 var (
@@ -42,10 +51,16 @@ var AppCmd = &cobra.Command{
 }
 
 func init() {
-
+	AppCmd.Flags().StringVar(&params.Namespace, namespaceFlag, "", "Namespace for the app")
+	AppCmd.Flags().StringVar(&params.Description, descriptionFlag, "", "Description of the app")
 }
 
 func runCmd(cmd *cobra.Command, args []string) error {
+	r := bufio.NewReader(os.Stdin)
+	return createApp(r)
+}
+
+func createApp(r *bufio.Reader) error {
 	dir, err := os.Getwd()
 	if err != nil {
 		return fmt.Errorf("unable to determine local directory: %w", err)
@@ -59,10 +74,38 @@ func runCmd(cmd *cobra.Command, args []string) error {
 	gitCommitter := repository.NewGitCommitter()
 	appSvc := app.NewCreator(gitCommitter)
 
-	_, err = appSvc.Create(repo, params.Name, "test-space", "This is a test")
+	if params.Namespace == "" {
+		fmt.Printf("Namespace (e.g. flux-system): ")
+
+		params.Namespace, err = readAndFormatInput(r, "namespace")
+		if err != nil {
+			return err
+		}
+	}
+
+	if params.Description == "" {
+		fmt.Printf("Description: ")
+
+		params.Description, err = readAndFormatInput(r, "description")
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = appSvc.Create(repo, params.Name, params.Namespace, params.Description)
 	if err != nil {
 		return fmt.Errorf("issue creating an app: %w", err)
 	}
 
 	return nil
+}
+
+func readAndFormatInput(r *bufio.Reader, field string) (string, error) {
+	input, err := r.ReadString('\n')
+	if err != nil {
+		return "", fmt.Errorf("issue reading input for %s: %w", field, err)
+	}
+
+	input = strings.Replace(input, "\n", "", -1)
+	return input, nil
 }
